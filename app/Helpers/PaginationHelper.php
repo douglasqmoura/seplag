c218a8728021535a2ad8e9b94d4de7d10bdc5b84
Douglas Moura <douglas@dabliotecnologia.com.br>
2025-04-01 16:07:08 -0400

    refatora helper de paginação

--- app/Helpers/PaginationHelper.php
+++ app/Helpers/PaginationHelper.php
@@ -2,12 +2,28 @@
 
 namespace App\Helpers;
 
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use Illuminate\Support\Collection;
+
 class PaginationHelper
 {
     public static function paginate($query, $resourceClass, $defaultPerPage = 10)
     {
         $perPage = request()->get('per_page', $defaultPerPage);
-        $paginator = $query->paginate($perPage)->appends(request()->query());
+        $currentPage = request()->get('page', 1);
+
+        if ($query instanceof Collection) {
+            $results = $query->forPage($currentPage, $perPage)->values();
+            $paginator = new Paginator(
+                $results,
+                $query->count(),
+                $perPage,
+                $currentPage,
+                ['path' => request()->url(), 'query' => request()->query()]
+            );
+        } else {
+            $paginator = $query->paginate($perPage)->appends(request()->query());
+        }
 
         return $resourceClass::collection($paginator);
     }
